a8676861e8d99a923091d9ddcc776b2b3d60f3e8
Danielle Puga <danielle.puga@gmail.com>
2021-10-10 15:31:56 -0700

    added user input variables

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,17 +1,27 @@
 //import 'package:dart_space_adventure/dart_space_adventure.dart'
 // as dart_space_adventure;
+import 'dart:io';
 
 void main(List<String> arguments) {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
+
   print('What is your name?');
-  print('Jane');
+
+  final name = stdin.readLineSync();
+
   print(
-      'Nice to meet you, Jane. My name is Eliza, I\'m an old friend of Alexa.');
-  print('Let\'s go on an adventure!');
-  print('Shall I randomly choose a planet for you to visit? (Y or N)');
+      'Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+
+  print('Let\'s go on an adventure!\n'
+      'Shall I randomly choose a planet for you to visit? (Y or N)');
+
+  final answer = stdin.readLineSync();
+
   print('Name the planet you would like to visit.');
-  print('Neptune');
-  print('Traveling to Neptune...');
-  print('Arrived at Neptune. A very cold planet, furthest from the sun');
+
+  final planetName = stdin.readLineSync();
+
+  print('Traveling to $planetName...');
+  print('Arrived at $planetName. A very cold planet, furthest from the sun');
 }
