3dab964990624c17b9118e511e4aa52592ca99c7
Danielle Puga <danielle.puga@gmail.com>
2021-10-10 20:12:59 -0700

    refactoring

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -16,17 +16,24 @@ void main(List<String> arguments) {
   print('Let\'s go on an adventure!\n'
       'Shall I randomly choose a planet for you to visit? (Y or N)');
 
-  final answer = stdin.readLineSync();
+  var answer;
 
-  if (answer == 'Y') {
-    print('Okay! Traveling to Mercury...\n'
-        'Arrived at Mercury. A very hot planet, closest to the sun.');
-  } else {
-    print('Name the planet you would like to visit.');
+  while (answer != 'Y' && answer != 'N') {
+    answer = stdin.readLineSync();
+    if (answer == 'Y') {
+      print('Okay! Traveling to Mercury...\n'
+          'Arrived at Mercury. A very hot planet, closest to the sun.');
+    } else if (answer == 'N') {
+      print('Name the planet you would like to visit.');
 
-    final planetName = stdin.readLineSync();
+      final planetName = stdin.readLineSync();
 
-    print('Traveling to $planetName...');
-    print('Arrived at $planetName. A very cold planet, furthest from the sun');
+      print('Traveling to $planetName...');
+      print(
+          'Arrived at $planetName. A very cold planet, furthest from the sun');
+    } else {
+      print('Sorry, I didn\'t get that.');
+      answer = stdin.readLineSync();
+    }
   }
 }
